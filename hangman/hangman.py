2647cbd1f3c0a695df504f683763a206936b4b65
ynnmuraii <tihookeanski.y.tanruuu@gmail.com>
2025-07-04 00:21:23 +0400

    beta version without status

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -4,16 +4,14 @@ from pathlib import Path
 
 def get_words_from_file(file_path: str) -> List[str]:
     file_path = Path("words.txt")
-    with file_path.open("r") as f:
+    with file_path.open("r", encoding="utf-8") as f:
         words = f.readlines()
     return [word.strip() for word in words if word.strip()]
 
 def get_random_word(words: List[str]) -> str:
     return random.choice(words)
  
-def hangman_status(word: str)
- 
-
+#def hangman_status(word: str)
 
 def alphabet() -> str:
     return 'абвгдеёжзийклмнопрстуфхчцшщъыьэюя'
@@ -24,7 +22,10 @@ def play_hangman():
     words = get_words_from_file("words.txt")
     word = get_random_word(words)
     tries = 8
-    while tries > 0 and not game_over:
+    used_letters = set()
+    game_over = False
+    print("Загаданное слово:", " _ " * len(word))
+    while tries > 0 and game_over == False:
         for tries in range(8, 0, -1):
             print(f"Осталось попыток: {tries}")
             
@@ -32,11 +33,17 @@ def play_hangman():
             if guess not in alphabet():
                 print("Неверный ввод. Введи букву из русского алфавита.")
                 continue
-            
+            if guess in used_letters:
+                print("Эта буква уже была использована. Попробуй другую.")
+                continue
+
             if guess in word:
                 print("Правильно!")
+                used_letters.add(guess)
+                
             elif guess not in word:
                 print("Неверно!")
+                used_letters.add(guess)
                 tries -= 1
             
             if tries == 0:
@@ -44,15 +51,13 @@ def play_hangman():
                 game_over = True
                 break
             
-            
-                
-            
+            if all(letter in used_letters for letter in word):
+                print("Поздравляю! Ты выиграл! Твоё слово: ", word)
+                game_over = True
+     
 
-
-
-    
 while True:
-    choice = input("Ещё раз? (y/n): ").strip().lower()
+    choice = input("Новая игра? (y/n): ").strip().lower()
     if choice == 'y':
         play_hangman()
     elif choice == 'n':
